30c2c3633b51f530045a3a0fe858536cab82d772
Padmanav Sahoo <108537776+padmanav15@users.noreply.github.com>
2024-08-21 11:56:32 +0530

    Update Admin.Master.cs

--- Admin.Master.cs
+++ Admin.Master.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,33 @@ namespace ECommerce_App
 {
     public partial class Admin : System.Web.UI.MasterPage
     {
+        SqlConnection conn;
         protected void Page_Load(object sender, EventArgs e)
         {
+            string cnf = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+            conn=new SqlConnection(cnf);
+            conn.Open();
+            if (Session["MyUser"]!=null)
+            {
+                string user_email = Session["MyUser"].ToString();
+                string q = "select * from user_account where acc_email='" + user_email + "'";
+                
+                SqlCommand cmd=new SqlCommand(q,conn);
+                SqlDataReader rdr = cmd.ExecuteReader();
+                rdr.Read();
+                string user=rdr["acc_user"].ToString();
+                Label1.Text = user;
+            }
+            else
+            {
+                Response.Write("<script>alert('You need to Login First!!');window.location.href='Login.aspx'</script>");
+            }
+        }
 
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            Response.Redirect("Login.aspx");
         }
     }
 }
