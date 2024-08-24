000b27b3c077d6be00ba56d49740ced8e31d1fb5
Padmanav Sahoo <108537776+padmanav15@users.noreply.github.com>
2024-08-24 13:01:14 +0530

    Add files via upload

---
+++ UserList.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ECommerce_App
+{
+    public partial class UserList : System.Web.UI.Page
+    {
+        SqlConnection conn;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string cnf = ConfigurationManager.ConnectionStrings["dbconn"].ToString();
+            conn=new SqlConnection(cnf);
+            conn.Open();
+            if(!IsPostBack)
+            {
+                FetchUserList();    
+            }
+
+        }
+        protected void FetchUserList()
+        {
+            string q = "exec UsersListProc ";
+            SqlCommand cmd = new SqlCommand(q, conn);   
+            SqlDataReader rdr = cmd.ExecuteReader();
+            GridView1.DataSource = rdr;
+            GridView1.DataBind();   
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            Label lbl=(Label) GridView1.Rows[e.RowIndex].FindControl("Label1");
+            string id=lbl.Text;
+            string q= "exec DeleteAccountById '" + id+"'";
+            SqlCommand c = new SqlCommand(q, conn);
+            c.ExecuteNonQuery();
+            FetchUserList();
+        }
+    }
+}
