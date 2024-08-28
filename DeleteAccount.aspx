6339b5439715958d09685c3f56eaea9243c83e59
Padmanav Sahoo <108537776+padmanav15@users.noreply.github.com>
2024-08-28 20:15:05 +0530

    Add files via upload

---
+++ DeleteAccount.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/User.Master" AutoEventWireup="true" CodeBehind="DeleteAccount.aspx.cs" Inherits="ECommerce_App.DeleteAccount" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:DataList ID="DataList1" runat="server" OnItemCommand="DataList1_ItemCommand">
+        <itemtemplate>
+            <div class="card" style="width: 18rem;">
+                <img height="100" width="100" src='<%#Eval("acc_profile") %>' class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><%#Eval("acc_user") %></h5>
+                    </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Email Id : <%#Eval("acc_email") %></li>
+                    <li class="list-group-item">Role : <%#Eval("acc_role") %></li>
+                </ul>
+                <div class="card-body">
+                    <asp:Button ID="Button1" class="btn btn-danger" CommandName="Delete" CommandArgument='<%#Eval("id") %>' runat="server" Text="Delete" />
+                </div>
+            </div>
+        </itemtemplate>
+    </asp:DataList>
+</asp:Content>
---
+++ DeleteAccount.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ECommerce_App
+{
+    public partial class DeleteAccount : System.Web.UI.Page
+        
+    {
+        SqlConnection conn;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string cnf = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+            conn= new SqlConnection(cnf);
+            conn.Open();
+            if(!IsPostBack)
+            {
+                FetchProfile();
+            }
+        }
+        protected void FetchProfile()
+        {
+            string em = Session["MyUser"].ToString();
+            string q = " exec FindProfileById  '" + em + "' ";
+            SqlDataAdapter ada= new SqlDataAdapter(q,conn);
+            DataSet ds=new DataSet();
+            ada.Fill(ds);
+            DataList1.DataSource = ds;  
+            DataList1.DataBind();   
+        }
+
+        protected void DataList1_ItemCommand(object source, DataListCommandEventArgs e)
+        {
+            if(e.CommandName =="Delete")
+            {
+               string id= e.CommandArgument.ToString();
+                string q = "Exec DeleteAccountById '"+id+"'";
+                SqlCommand cmd = new SqlCommand(q,conn);
+                cmd.ExecuteNonQuery();
+                Response.Write("<script>alert('Account Deleted Successfully');window.location.href='Login.aspx'</script>");
+                
+            }
+        }
+    }
+}
